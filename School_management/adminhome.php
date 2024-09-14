392e96bdbe42618c9be006f49e8dd683de3088c9
Tharaa17 <tharade1117@gmail.com>
2024-09-15 00:39:29 +0530

    w

--- School_management/adminhome.php
+++ School_management/adminhome.php
@@ -18,10 +18,73 @@ session_start();
 <head>
     <meta charset="utf-8">
     <title>Admin Dashboard</title>
-    <a href="logout.php">Logout</a>
+    <link rel="stylesheet" type="text/css" href="admin.css">
+    
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <h1>Admin Home</h1>
+   <header class="header">
+       <a href="">Admin Dashboard</a>
+       
+       <div>
+         <a href="logout.php" class="btn btn-primary">Logout</a>
+       </div>
+
+
+   </header>
+   <aside>
+       <ul>
+           <li>
+            <a href="">Admission</a>
+           </li>
+
+           <li>
+            <a href="">Add Student</a>
+           </li>
+
+           <li>
+            <a href="">View Student</a>
+           </li>
+
+           <li>
+            <a href="">Add Teacher</a>
+           </li>
+
+           <li>
+            <a href="">View Teacher</a>
+           </li>
+
+           <li>
+            <a href="">Add Courses</a>
+           </li>
+
+           <li>
+            <a href="">View Courses</a>
+           </li>
+
+
+       </ul>
+   </aside>
+
+   <div class="content">
+    <h1>
+    Lorem ipsum odor amet, consectetuer adipiscing elit.
+    </h1>
+
+    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ante nulla erat enim lobortis potenti sed porttitor nisi. Adipiscing vestibulum ipsum etiam pellentesque ligula. Facilisi nec vivamus laoreet torquent integer sapien fames. Et class vel aptent, vitae pharetra conubia nam imperdiet. Eu pellentesque consequat penatibus lectus molestie ipsum. Elit aliquam odio; porttitor massa odio elit gravida nec. Class fermentum sodales, hendrerit aenean nullam sem. Dui diam arcu diam purus tempor class maximus nullam nascetur.</p>
+
+   </div>
+
+
+
+
 </body>
 </html>
--- School_management/studenthome.php
+++ School_management/studenthome.php
@@ -19,11 +19,56 @@ session_start();
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Student Dashboard</title>
-    <a href="logout.php">Logout</a>
+    <title>Admin Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="admin.css">
+    
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 
 <body>
-    <h1>Student Home</h1>
+   <header class="header">
+       <a href="">Student Dashboard</a>
+       
+       <div>
+         <a href="logout.php" class="btn btn-primary">Logout</a>
+       </div>
+
+
+   </header>
+   <aside>
+       <ul>
+           <li>
+            <a href="">Mu Courses</a>
+           </li>
+
+           <li>
+            <a href="">My Results</a>
+           </li>
+
+           
+
+
+       </ul>
+   </aside>
+
+   <div class="content">
+    <h1>
+    Lorem ipsum odor amet, consectetuer adipiscing elit.
+    </h1>
+
+    <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Ante nulla erat enim lobortis potenti sed porttitor nisi. Adipiscing vestibulum ipsum etiam pellentesque ligula. Facilisi nec vivamus laoreet torquent integer sapien fames. Et class vel aptent, vitae pharetra conubia nam imperdiet. Eu pellentesque consequat penatibus lectus molestie ipsum. Elit aliquam odio; porttitor massa odio elit gravida nec. Class fermentum sodales, hendrerit aenean nullam sem. Dui diam arcu diam purus tempor class maximus nullam nascetur.</p>
+
+   </div>
+
+
+
+
 </body>
 </html>
